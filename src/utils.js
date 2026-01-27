0ab419fff0e4c49aa966e3bfc9fc802f1d4d77a0
Johnny Meng <jmyw139@gmail.com>
2026-01-26 23:58:02 -0500

    Changed model

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY })
 
 export async function makeAPICall(notes) {
     const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
+        model: "gemini-2.5-flash-lite",
         contents: "Explain how AI works in a few words"
     })
 
